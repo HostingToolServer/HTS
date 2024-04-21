65d1d44664a654879cdc23dfc09ec7ae766ee0f7
Hosting.net <162243603+HostingToolServer@users.noreply.github.com>
2024-04-21 16:43:37 -0500

    Add files via upload

---
+++ script/guardar.php
@@ -0,0 +1,8 @@
+<?php
+    $conectar = new MySQLi("localhost", "root", "", "hostingtoolserver");
+
+    $consulta="INSERT INTO usuarios VALUES('', '".$_POST["name"]."', '".$_POST["email"]."', '".$_POST["password"]."')";
+    $consulBD=mysqli_query($conectar, $consulta);
+
+    header("location:../index.php");
+?>
